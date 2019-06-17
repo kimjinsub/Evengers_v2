3b67862a76c1c2fd8a5d088c1aee0c0185fea178
jinsub <kimjinsub01@naver.com>
2019-06-17 20:50:44 +0900

    sry 진섭

--- Evengers_v2/src/main/java/com/event/evengers_v2/userClass/WebSocket.java
+++ Evengers_v2/src/main/java/com/event/evengers_v2/userClass/WebSocket.java
@@ -16,7 +16,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
-@Controller
 @ServerEndpoint(value="/webSocket")
 public class WebSocket {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
